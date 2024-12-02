4f641eaf49748444bad93b074ab80f693fd1caea
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-12-02 14:43:11 -0500

    add day 1 to scripts, compiled day 1 js file

---
+++ day1/day1.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+function main() {
+    var data = formatData(readInput());
+    var distance = get_distance_from_pairs(data);
+    console.log("answer for part 1: ".concat(distance));
+}
+function readInput() {
+    var input = "".concat(__dirname, "/input.txt");
+    return fs.readFileSync(input, { encoding: "utf8", flag: "r" });
+}
+/**
+ * Format the data from the string that readFileSync returns into something we can use to solve today's puzzles.
+ * @param data
+ * @returns
+ */
+function formatData(data) {
+    return create_pairs_array(split_lines_and_sort(data));
+}
+/**
+ * Create two lists of ids, sort them, and return them as a combined array.
+ * @param data String of input.txt content.
+ * @returns array
+ */
+function split_lines_and_sort(data) {
+    var left_side = [];
+    var right_side = [];
+    data.split(/\r?\n/).forEach(function (line) {
+        var ids = line.split(/\s+/);
+        left_side.push(parseInt(ids[0]));
+        right_side.push(parseInt(ids[1]));
+    });
+    left_side.sort();
+    right_side.sort();
+    return [left_side, right_side];
+}
+/**
+ * Combine two arrays into a single array of id pairs.
+ * @param data Arrays of ids that need to be combined into a single id pairs array.
+ * @returns array
+ */
+function create_pairs_array(data) {
+    var id_pairs = data[0].map(function (left_side_number, index) {
+        return [left_side_number, data[1][index]];
+    });
+    return id_pairs;
+}
+/**
+ * Calculate the sum of the distances of each id pair to get the answer for part 1.
+ * @param pairs Nested array of id pair arrays.
+ * @returns number
+ */
+function get_distance_from_pairs(pairs) {
+    var distance = 0;
+    pairs.forEach(function (id_pair) {
+        distance += Math.abs(id_pair[0] - id_pair[1]);
+    });
+    return distance;
+}
+main();
