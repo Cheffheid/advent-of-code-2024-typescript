7966db2fad8bd36cc0cd1f6957bbdc64e2e7b550
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-12-03 16:17:50 -0500

    day 3 - part 1 solved

---
+++ day3/day3.ts
@@ -0,0 +1,27 @@
+import readInput from "../utils/readInput";
+
+function main() {
+  const memory_data = readInput(__dirname);
+  const part_one_regex = /mul\((\d+,\d+)\)/gi;
+
+  const multiplications = memory_data.matchAll(part_one_regex);
+  const part_1_multiplication_sum = run_multiplications(multiplications);
+
+  console.log(`answer for part 1: ${part_1_multiplication_sum}`);
+}
+
+function run_multiplications(
+  multiplication_matches: RegExpStringIterator<RegExpExecArray>
+) {
+  let sum = 0;
+
+  for (const match of multiplication_matches) {
+    const digits = match[1].split(",");
+
+    sum += parseInt(digits[0]) * parseInt(digits[1]);
+  }
+
+  return sum;
+}
+
+main();
